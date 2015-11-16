ddb1b2ca75e2463121847cb7aede0fa300bfbbc4
ChalkPE <chalkpe@gmail.com>
2015-11-16 22:56:15 +0900

    Initial commit

--- src/main/java/pe/chalk/minecraft/OnlineDemangler.java
+++ src/main/java/pe/chalk/minecraft/OnlineDemangler.java
@@ -1,8 +1,22 @@
+/*
+ * Copyright 2015 ChalkPE
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package pe.chalk.minecraft;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import org.jsoup.Jsoup;
 
 /**
  * @author ChalkPE <chalkpe@gmail.com>
@@ -11,11 +25,12 @@ import java.net.URL;
 public class OnlineDemangler {
     private OnlineDemangler(){}
 
-    public static String demangle(String input) throws IOException {
-        final URL demanglerUrl = new URL("http://demangler.com/");
-        HttpURLConnection connection = (HttpURLConnection) demanglerUrl.openConnection();
-
-        connection.setDoOutput(true);
-        connection.setRequestMethod("POST");
+    public static String demangle(final String input){
+        try{
+            return Jsoup.connect("http://demangler.com/").data("input", input).post().select("div.responseSection > pre").first().text();
+        }catch(Exception e){
+            e.printStackTrace();
+            return null;
+        }
     }
 }
